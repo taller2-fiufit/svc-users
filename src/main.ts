f5c6a138b40e92dc194738ad6036969198800642
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-04-01 13:42:04 -0300

    Agrego OpenAPI doc

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+//TODO: Eliminar cookie session
+//const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /* app.use(cookieSession({
+    keys: ['lalalalal']
+  }))
+  */ 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
     })
   )
+
+  const config = new DocumentBuilder()
+    .setTitle('Users')
+    .setDescription('Documentación de API Users')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
