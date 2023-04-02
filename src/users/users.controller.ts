ba234cebede095f003893801516130a8bf456316
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-04-02 15:43:52 -0300

    Cambio la semantica de la API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,7 +9,7 @@ import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './users.entity';
 import { AuthGuard } from '../guards/auth.guard';
 
-@Controller('auth')
+@Controller('')
 @Serialize(UserDto)
 export class UsersController {
     constructor(
@@ -17,47 +17,47 @@ export class UsersController {
         private authService: AuthService
     ) {}
     
-    @Post('signup')
-    async signup(@Body() body: CreateUserDto, @Session() session: any) {
-       const user = await this.authService.signup(body.email, body.password);
-       session.userId = user.id;
-       return user;
-    }
-
-    @Post('signin')
+    @Post('tokens')
     async signin(@Body() body: CreateUserDto, @Session() session: any) {
         const user = await this.authService.signin(body.email, body.password);
         session.userId = user.id;
         return user;
     }
 
-    @Post('signout')
+    @Delete('tokens')
     signout(@Session() session: any) {
         return session.userId = null
     }
 
-    @Get('whoami')
+    @Post('users')
+    async signup(@Body() body: CreateUserDto, @Session() session: any) {
+       const user = await this.authService.signup(body.email, body.password);
+       session.userId = user.id;
+       return user;
+    }
+
+    @Get('users/me')
     @UseGuards(AuthGuard)
     whoAmI(@CurrentUser() user: User) {
         return user;
     }
 
-    @Get(':id')
+    @Get('users/:id')
     findUser(@Param('id') id: string) {
         return this.usersService.findOne(parseInt(id));
     }
 
-    @Get()
+    @Get('/users')
     findAllUsers(@Query('email') email: string) {
         return this.usersService.find(email);
     }
 
-    @Delete(':id')
+    @Delete('users/:id')
     deleteUser(@Param('id') id: string) {
         return this.usersService.remove(parseInt(id));
     }
 
-    @Patch(':id')
+    @Patch('users/:id')
     updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
         return this.usersService.update(parseInt(id), body)
     }
