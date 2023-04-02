6d076748bbd9e9af3819bf402479337987e7fdb9
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-04-02 16:12:23 -0300

    cambios de doc en la docu

--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDto {
     @IsEmail()
     @IsOptional()
+    @ApiProperty()
     email: string;
 
     @IsString()
     @IsOptional()
+    @ApiProperty()
     password: string;
 }
