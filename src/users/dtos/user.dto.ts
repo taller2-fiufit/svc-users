650a4afec9429295a96a3f1cd249cfa70f991576
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-04-02 16:11:35 -0300

    se modifica la documentacion

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
 
 export class UserDto {
     @Expose()
+    @ApiProperty()
     id: number;
 
     @Expose()
+    @ApiProperty()
     email: string;
 }
