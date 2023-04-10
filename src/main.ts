bf1d1640eac2890a57467664526209469d1de727
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-04-10 16:18:01 -0300

    Se cambia endpoint de swagger

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .addTag('users')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
