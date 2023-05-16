bc63f8b8ba592f99945b70a3a0326d6ff559a360
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-05-16 18:26:23 -0300

    Pruebi

--- src/producer/producer.module.ts
+++ src/producer/producer.module.ts
@@ -35,5 +35,6 @@ export class ProducerModule {
       accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
       secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
     });
+    console.log(myConfig);
   }
 }
