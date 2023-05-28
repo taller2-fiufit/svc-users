20e1eee003ca3045d08fdd9d643bcb8d9c863305
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2023-05-27 22:04:47 -0300

    Exclude health endpoint from docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiExcludeEndpoint(true)
   @Get('health')
   is_healthy() {
     return "I'm alive!";
