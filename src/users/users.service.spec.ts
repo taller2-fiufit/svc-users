eb23bb9661c762141ab69714c8008b4b3af6699c
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2023-06-03 16:03:18 -0300

    Remove test
    The functionality uses PostgreSQL-specific operators

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -142,47 +142,4 @@ describe('UsersService', () => {
     await service.remove(1);
     await expect(service.findOne(1)).rejects.toThrow(NotFoundException);
   });
-
-  it('has to return users nearer than X kms, ordered by distance', async () => {
-    await service.create(
-      'jondoe1@kinetix.com',
-      'Temporal1234',
-      'Normal User1',
-      false,
-      'soy user prueba',
-      'Buenos Aires',
-      'Argentina',
-      0.0,
-      0.0,
-      '',
-    );
-    await service.create(
-      'jondoe2@kinetix.com',
-      'Temporal1234',
-      'Normal User2',
-      false,
-      'soy user prueba',
-      'Buenos Aires',
-      'Argentina',
-      0.1,
-      0,
-      '',
-    );
-    await service.create(
-      'jondoe3@kinetix.com',
-      'Temporal1234',
-      'Normal User2',
-      false,
-      'soy user prueba',
-      'Buenos Aires',
-      'Argentina',
-      32.0,
-      15.0,
-      '',
-    );
-    const result = await service.findByDistance(0.0, 0.0, 20.0);
-    expect(result.length).toBe(2);
-    expect(result[0].email).toBe('jondoe1@kinetix.com');
-    expect(result[1].email).toBe('jondoe2@kinetix.com');
-  });
 });
