e9ecf64428292439f85c9cec8e97fedab758a0f7
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-06-04 18:03:41 -0300

    Pruebo que se de el error en testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -15,8 +15,6 @@ describe('AuthService', () => {
   let mockProducerService: Partial<ProducerService>;
   let users: User[];
 
-  jest.useFakeTimers();
-
   beforeEach(async () => {
     users = [];
     mockProducerService = {
