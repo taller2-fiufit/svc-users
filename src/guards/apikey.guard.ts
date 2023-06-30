3cc42c625f959f5412efe2b4d541e97a8f99d66d
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-06-29 22:45:12 -0300

    Agrego debug logs.

--- src/guards/apikey.guard.ts
+++ src/guards/apikey.guard.ts
@@ -1,18 +1,24 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+
+  private readonly logger = new Logger(ApiKeyGuard.name);
+
   async canActivate(context: ExecutionContext) {
     const apikey = context.switchToHttp().getRequest().headers['x-apikey'];
     if (
       process.env.NODE_ENV == 'development' ||
       process.env.NODE_ENV == 'test'
     ) {
+      this.logger.debug('Request autorizado por entorno no productivo');
       return true;
     }
     if (!apikey || apikey != process.env.USERS_API_KEY) {
+      this.logger.debug(`Request rechazado - ApiKey: ${apikey}`);
       return false;
     }
+    this.logger.debug('Request autorizado por entorno');
     return true;
   }
 }
